8567a7a271a9b37f820e3a6d25fa496a0276a5c0
pavankumar98 <39565289+pavankumar98@users.noreply.github.com>
2018-12-28 21:48:09 +0530

    Add files via upload

---
+++ eveodd.py
@@ -0,0 +1,8 @@
+input_num = float(input())
+
+if input_num%2 == 0:
+	print("even")
+elif input_num%2 == 1:
+	print("odd")
+else:
+	print("invalid")
---
+++ num_id.py
@@ -0,0 +1,8 @@
+input_num = float(input())
+
+if input_num == 0:
+	print("zero")
+elif input_num > 0:
+	print("positive")
+elif input_num < 0:
+	print("negative")
