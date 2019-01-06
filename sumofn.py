d4a4d39b95913e65924e4d59d2969c3fdec4c493
pavankumar98 <39565289+pavankumar98@users.noreply.github.com>
2019-01-06 12:36:41 +0530

    Add files via upload

---
+++ sumofn.py
@@ -0,0 +1,15 @@
+input_num = int(input('calculate up to?'))
+
+def sum_of_num(n):
+        num = 0
+        for i in range(1,n+1):
+                num += i
+        print (num)
+
+                      
+		
+
+(sum_of_num(input_num))
+
+
+
